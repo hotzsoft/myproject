13d72bd23c05371eeb80d87174b0648d2da58ddc
hotzsoft <hotzsoft.sales@gmail.com>
2019-04-17 15:11:13 +0800

    java.  Hello World type

---
+++ src/main/java/com/trato/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.trato;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        new Person().hello();
+    Person p = new Person();
+    p.hello();
+    int age = 19;
+    char c = '我';
+    byte b = 120;
+    float weight = 72.3f;
+    double height = 1.72;
+    boolean adult = true;
+    boolean enroll = false;
+    }
+
+
+}
